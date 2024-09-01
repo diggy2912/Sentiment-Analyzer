73ad53909d7dca1cefaaae2499c3541675f29879
Nikhil241103 <pekacoclover@gmail.com>
2024-09-01 18:24:02 +0530

    managed failure case whie getting tweet

--- extension/popup.js
+++ extension/popup.js
@@ -1,9 +1,11 @@
 const tweetText = document.getElementById('tweet-text')
+const tweetTextHeader = document.getElementById('tweet-text-header')
 const sentiment = document.getElementById('sentiment')
 const extractTweetButton = document.getElementById('extract-tweet')
 const getSentimentButton = document.getElementById('get-sentiment')
 
 tweetText.style.display = 'none'
+tweetTextHeader.style.display = 'none'
 sentiment.style.display = 'none'
 getSentimentButton.style.display = 'none'
 
@@ -12,15 +14,21 @@ extractTweetButton.addEventListener('click', () => {
     if (tabs.length > 0) {
       chrome.tabs.sendMessage(tabs[0].id, { action: 'getTweetText' }, (response) => {
         tweetText.style.display = 'block'
-        extractTweetButton.style.display = 'none'
-        getSentimentButton.style.display = 'block'
-        tweetText.innerText = response ? response.tweetText : 'Failed to retrieve tweet text!'
+        tweetTextHeader.style.display = 'block'
+
+        if (response) {
+          extractTweetButton.style.display = 'none'
+          getSentimentButton.style.display = 'block'
+          tweetText.innerText = response.tweetText
+        } else {
+          tweetText.innerText = 'Failed to retrieve tweet text!'
+        }
       })
     }
   })
 })
 
-getSentimentButton.addEventListener('click', () => {  
+getSentimentButton.addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: 'analyzeSentiment' }, (respose) => {
     getSentimentButton.style.display = 'none'
     sentiment.style.display = 'flex'
